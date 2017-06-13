303912730699f6a05e8c5a5c99a5461e966a28db
Ravi Shankar Jha <ravi.sun66@gmail.com>
2017-06-13 13:06:12 +0530

    add validation to name

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "Users can create new projects" do
-  scenario "with valid attributes" do
+  before do
     visit "/"
-    click_link "New Project"
+    click_link "New Project"    
+  end
+  scenario "with valid attributes" do
     name = "My Homework"
     description = "My home task module"
     fill_in "Name", with: name
@@ -14,5 +16,10 @@ RSpec.feature "Users can create new projects" do
     expect(page.current_url).to eq project_url project
     title = "#{name} | Ticketee"
     expect(page).to have_title title 
+  end
+  scenario "when providing invalid attributes" do
+    click_button "Create Project"
+    expect(page).to have_content "Project has not been created."
+    expect(page).to have_content "Name can't be blank"
   end
 end
