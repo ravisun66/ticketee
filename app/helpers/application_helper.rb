b6eaf6b026c41e95994a1dcfb5cbe28a7ba08b2d
Ravi Shankar Jha <ravi.sun66@gmail.com>
2017-06-13 11:51:09 +0530

    add title for project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*title)
+    unless title.empty?
+      content_for :title do
+        (title << "Ticketee").join " | "
+      end
+    end
+  end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -4,9 +4,15 @@ RSpec.feature "Users can create new projects" do
   scenario "with valid attributes" do
     visit "/"
     click_link "New Project"
-    fill_in "Name", with: "My Homework"
-    fill_in "Description", with: "My home task module"
+    name = "My Homework"
+    description = "My home task module"
+    fill_in "Name", with: name
+    fill_in "Description", with: description
     click_button "Create Project"
     expect(page).to have_content "Project has been created."
+    project = Project.find_by(name: name)
+    expect(page.current_url).to eq project_url project
+    title = "#{name} | Ticketee"
+    expect(page).to have_title title 
   end
 end
